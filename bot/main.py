c58eee6a0f11378b575706938686f5caee5259ea
root <root@tshi360desktop.localdomain>
2026-03-10 03:21:16 +0900

    add bot

---
+++ bot/main.py
@@ -0,0 +1,21 @@
+import discord
+from config import TOKEN
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f"log in : {client.user}")
+
+@client.event
+async def on_message(message):
+        
+    if message.author == client.user:
+        return
+
+    await message.channel.send(message.content)
+
+client.run(TOKEN)
