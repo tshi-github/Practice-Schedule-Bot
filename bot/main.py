b1f94725b05e7c4fca032cefcc3de8c2d4e24834
root <root@tshi360desktop.localdomain>
2026-03-15 00:15:25 +0900

    完全に会議室の予約方針からカレンダー連携に移行

--- bot/main.py
+++ bot/main.py
@@ -1,21 +1,48 @@
 import discord
+from discord.ext import commands
 from config import TOKEN
+from datetime import datetime
 
 intents = discord.Intents.default()
 intents.message_content = True
+intents.reactions = True
+intents.members = True
 
-client = discord.Client(intents=intents)
+bot = commands.Bot(command_prefix="!", intents=intents)
 
-@client.event
+@bot.event
 async def on_ready():
-    print(f"logged in : {client.user}")
+    print(f"logged in : {bot.user}")
 
-@client.event
-async def on_message(message):
-        
-    if message.author == client.user:
+@bot.command()
+async def event(ctx, date, time, *, content):
+
+    message = await ctx.send(
+        f"{date} {time}\n{content}"
+    )
+
+    await message.add_reaction("⭕")
+    await message.add_reaction("❌")
+    await message.add_reaction("🔺")
+
+    year = datetime.now().year
+    month, day = map(int, date.split("/"))
+    start, end = time.split("-")
+
+    start_dt = datetime.strptime(f"{year}-{month}-{day} {start}", "%Y-%m-%d %H:%M")
+    end_dt = datetime.strptime(f"{year}-{month}-{day} {end}", "%Y-%m-%d %H:%M")
+
+    print("開始時間:", start_dt.isoformat())
+    print("終了時間:", end_dt.isoformat())
+
+@bot.event
+async def on_reaction_add(reaction, user):
+
+    if user.bot:
         return
+    
+    emoji = reaction.emoji
+    if emoji == "⭕":
+        print(f"{user} ({user.id})")
 
-    await message.channel.send(message.content)
-
-client.run(TOKEN)
+bot.run(TOKEN)
