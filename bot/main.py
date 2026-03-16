10c24ea0154a96bd8a2fcfa76ecf5756202ddb1f
root <root@tshi360desktop.localdomain>
2026-03-17 00:16:36 +0900

    Renderの自動停止阻止のコードを追加

--- bot/main.py
+++ bot/main.py
@@ -1,5 +1,8 @@
 import discord
 from discord.ext import commands
+import os
+from flask import Flask
+from threading import Thread
 from config import TOKEN
 from datetime import datetime
 
@@ -44,5 +47,21 @@ async def on_reaction_add(reaction, user):
     emoji = reaction.emoji
     if emoji == "⭕":
         print(f"{user} ({user.id})")
+
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "Bot is running!"
+
+def run():
+    port = int(os.environ.get("PORT", 3000))
+    app.run(host="0.0.0.0", port=port)
+
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
+
+keep_alive()
 
 bot.run(TOKEN)
