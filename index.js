0045a5004ba17453b7799b52781418463e5486c5
root <root@tshi360desktop.localdomain>
2026-03-20 14:29:27 +0900

    change index.js

--- index.js
+++ index.js
@@ -2,11 +2,15 @@ import { Client, GatewayIntentBits } from "discord.js";
 import express from "express";
 
 const client = new Client({
-  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages],
+  intents: [
+    GatewayIntentBits.Guilds, 
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ],
 });
 
 // Discord Botログイン
-client.login(process.env.DISCORD_TOKEN);
+console.log("TOKEN:", process.env.DISCORD_TOKEN ? "OK" : "NG");
 
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`);
