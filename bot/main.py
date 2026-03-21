bf93c3c36197967a0ffc22efc7edcff03c32ddc1
root <root@tshi360desktop.localdomain>
2026-03-21 20:25:50 +0900

    main.py のバグを修正

--- bot/main.py
+++ bot/main.py
@@ -11,7 +11,7 @@ intents.message_content = True
 intents.reactions = True
 intents.members = True
 
-bot = commands.Bot(command_prefix="!")
+bot = commands.Bot(command_prefix="!", intents=intents)
 
 @bot.event
 async def on_ready():
