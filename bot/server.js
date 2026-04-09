45bd6fad46ad048d6aedf9675731f4eaca088887
tshi-github <ebideit360@gmail.com>
2026-04-10 08:52:14 +0900

    port番号変更

--- bot/server.js
+++ bot/server.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const express = require('express');
 const app = express();
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 10000;
 
 app.get('/', (req, res) => {
   res.send('OK');
